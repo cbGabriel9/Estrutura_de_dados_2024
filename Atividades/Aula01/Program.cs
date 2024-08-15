273efc95d94487bb5d28f8751459ae84d9f2a5c4
Gabriel Ceron Bianchi <431191@unoesc.lan>
2024-08-14 22:05:28 -0300

    Início da programação

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Declarando variável
+
+const int DAYS_IN_WEEK = 10;
+
+//Declarando variável 
+
+int number = 0;
+
+//Imprimindo String com variável
+
+Console.WriteLine("Temos {0} dias na semana", DAYS_IN_WEEK);
+
+//Imprimindo String concatenada
+
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
+
+                                                

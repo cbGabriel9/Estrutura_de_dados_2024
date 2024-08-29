a98c5cdfe0eefddf7f4583d83851728d2fa2f7b5
Gabriel Ceron Bianchi <431191@unoesc.lan>
2024-08-28 21:26:18 -0300

    Conteúdo feito em Aula

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1 = new Person("Maurício Gonzatto", 40);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person();
+person2.Name = "Gabriel Ceron Bianchi";
+person2.Age = 70;
+Console.WriteLine(person2.Name);
+
+Person person3 = new Person()
+{
+    Name = "Jordan da Silva",
+    Age = 80
+};
+Console.WriteLine(person3.Name);

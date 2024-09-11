1ad0f39a8adbddd0481fa125a35eba5bb32fba85
Gabriel Ceron Bianchi <431191@unoesc.lan>
2024-09-11 20:06:08 -0300

    Primeira parte - Lista Genérica

---
+++ Atividades/ListaGenerica/Program.cs
@@ -0,0 +1,16 @@
+﻿List<double> numbers = [];
+
+do
+{
+    Console.WriteLine("Informe um número");
+    string? numberStr = Console.ReadLine();
+
+    // Validando a entrada do usuário
+
+    if (!double.TryParse(numberStr, out double number))
+    
+        break;
+    
+    numbers.Add(number);
+    Console.WriteLine($"A média é de {numbers.Average()}");
+} while (true);

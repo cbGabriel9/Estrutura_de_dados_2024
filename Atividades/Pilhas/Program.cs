c6cfb8d5a91f0e16623eaf6a7e33fa07b9cc5cb4
Gabriel Ceron Bianchi <431191@unoesc.lan>
2024-09-11 22:05:52 -0300

    Fechamento

---
+++ Atividades/Pilhas/Program.cs
@@ -0,0 +1,45 @@
+﻿// Basicamente as pilhas trabalham com uma estrutura LIFO -> Last In First Out. Três métodos importantes:
+
+// Push() -> Insere um elemento no topo da pilha
+// POP() -> Remove um elemento do topo e o retorna
+//Peek() -> Retorna o elemento do topo sem removê-lo
+
+// Invertendo as bolas 
+
+// Stack<char> caracteres = [];
+
+// foreach(char c in "Gusta do Pé Quente")
+// {
+//     caracteres.Push(c);
+// }
+
+// // Desempilhando
+
+// string invertido = string.Empty;
+// while(caracteres.Count > 0)
+// {
+//     invertido += caracteres.Pop();
+// }
+
+// Console.WriteLine(invertido);
+
+Stack<char> palavra = [];
+string invertido = string.Empty;
+Console.WriteLine("Digite uma palavra:");
+string? entrada = Console.ReadLine();
+
+foreach (char c in entrada)
+{
+    palavra.Push(c);
+}
+
+while(palavra.Count > 0)
+{
+    invertido += palavra.Pop();
+}
+
+if(invertido == entrada){
+    Console.WriteLine("Esta palavra é um palíndromo");
+} else {
+    Console.WriteLine("Esta palavra não é um palíndromo");
+}

0cdc96f2603de46f40ddc48a300f74b88df6ebff
Gabriel Ceron Bianchi <142252182+Gabrieloprogramador19@users.noreply.github.com>
2024-09-17 16:14:29 -0300

    AlteraçõesComuns

---
+++ Atividades/ListaLinkadas/Poltronas.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ListasLinkadas
+{
+    public class Poltronas
+    {
+        public string? Letra { get; set; }
+        public int Numero { get; set; }
+    };
+}
---
+++ Atividades/ListaLinkadas/Program.cs
@@ -0,0 +1,7 @@
+﻿using ListasLinkadas;
+using System.Collections.Generic;
+using System;
+
+LinkedList<string> poltronas = [];
+
+poltronas.AddLast(new Poltronas());

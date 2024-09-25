e497801e4dc8806c58874b5fe8d1957980aee79b
Gabriel Ceron Bianchi <142252182+Gabrieloprogramador19@users.noreply.github.com>
2024-09-25 16:31:03 -0300

    Dictionary1

---
+++ Aulas/Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+Dictionary<int, string> dict = new()
+{
+    {0, "Tomate"},
+    {1, "Queijo"},
+    {2, "Arroz"},
+    {3, "Feijão"}
+
+
+};
+
+dict.Add(4, "Gabriel");
+dict.Add(5, "Sophia");
+dict.Add(6, "Ruby");
+
+foreach (KeyValuePair<int, string> item in dict)
+{
+    Console.WriteLine("Key: {0} | Valor: {1}", item.Key, item.Value);
+}

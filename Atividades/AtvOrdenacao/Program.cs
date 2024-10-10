2c7f798acd511b516d69b1d12f03e4160d427f51
Gabriel Ceron Bianchi <431191@unoesc.lan>
2024-10-09 21:50:37 -0300

    AulaOrdenacao

---
+++ Atividades/AtvOrdenacao/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Metadata;
+using AtvOrdenacao;
+
+string[] names = new string[3]{"Gabriel", "Amanda", "Thiago"};
+
+AtvOrdenacao.InsertionSort.Sort(names);
+
+foreach(string name in names)
+    System.Console.WriteLine(name);
---
+++ Aulas/AlgoritmosOrdenacao/BubbleSort/Program.cs
@@ -0,0 +1,17 @@
+﻿using BubbleSort;
+
+int[] inteiros = {0, 12, 7, 1, 2, 8};
+
+BubbleSort.BubbleSort.Sort(inteiros);
+
+BubbleSort.BubbleSort.Sort<int>(inteiros);
+
+BubbleSort.BubbleSort.Print(inteiros);
+
+System.Console.WriteLine("Teste Char");
+
+char[] chars = ['x', 'z', 'b', 'c', 'a'];
+
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
